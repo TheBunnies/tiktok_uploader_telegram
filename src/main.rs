7f69952942bafa22e2f5ee137c72113748f7df37
Alexei Ulanov <lucky-stees@yandex.ru>
2022-02-18 18:25:25 +0300

    Add error handling and moved regex to shared state

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use teloxide::{prelude2::*, types::InputFile};
 use regex::*;
 use tiktok_uploader_telegram::tiktok::*;
 
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref REGEX: Regex = Regex::new(r"http(s|)://.*(tiktok).com[^\s]*").unwrap();
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,21 +15,34 @@ async fn main() {
     let bot = Bot::from_env().auto_send();
 
     teloxide::repls2::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        let regex = Regex::new(r"http(s|)://.*(tiktok).com[^\s]*").unwrap();
         let message_text = message.text().unwrap();
-        if regex.is_match(message_text) {
-            let resp= Response::new(message_text).await.unwrap();
-            resp.download_video().await;
-            let file = InputFile::file(resp.get_file_name());
+        if REGEX.is_match(message_text) {
+
+            let resp=  Response::new(message_text).await.ok();
+            let response : Response;
+
+            if let None = resp {
+                bot.send_message(message.chat_id(), "Your tiktok link does not lead to anything.").await?;
+                return respond(());
+            }
+            else {
+                response = resp.unwrap();
+            }
+            let err = response.download_video().await.ok();
+            if let None = err {
+                bot.send_message(message.chat_id(), "Could not download the specified video, please try again later.").await?;
+                return respond(());
+            }
+            let file = InputFile::file(response.get_file_name());
             let content = format!("Author: {}\nDescription: {}\nDuration: {}\nDate uploaded: {}", 
-                resp.aweme_detail.author.unique_id, 
-                resp.get_description(), 
-                resp.get_duration(), 
-                resp.get_date_created());
+            response.aweme_detail.author.unique_id, 
+                response.get_description(), 
+                response.get_duration(), 
+                response.get_date_created());
 
             bot.send_message(message.chat_id(), content).await?;
             bot.send_video(message.chat_id(), file).await?;
-            resp.delete_video().await;
+            response.delete_video().await.expect("Could not delete the video... Starting to panic!!!");
 
         } else {
             bot.send_message(message.chat_id(), "Not a tiktok link.").await?;
