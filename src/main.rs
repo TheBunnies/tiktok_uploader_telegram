6a7a60739df91c686980e85be61065145c91329e
Alexei Ulanov <lucky-stees@yandex.ru>
2022-08-18 20:17:17 +0300

    Rewrite for a new version of teloxide

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use teloxide::{prelude2::*, types::InputFile};
+use teloxide::{prelude::*, types::InputFile};
 use regex::*;
 use chrono::Local;
 use tiktok_uploader_telegram::tiktok::*;
@@ -30,7 +30,7 @@ async fn main() {
             if let None = resp {
                 let user = message.from().unwrap();
                 let last_name = user.last_name.to_owned().unwrap_or(String::new());
-                log::info!("{} Rejected request `Tiktok link does not lead to anything`: {} From user: {} {}/{}", Local::now(), message_text, user.first_name, last_name, user.id);
+                log::info!("{} Rejected request `Tiktok link does not lead to anything`: {} From user: {} {}/{}", Local::now(), message_text, user.first_name, last_name, user.id.0);
                 bot.send_message(message.chat_id(), "Your tiktok link does not lead to anything.").await?;
                 return respond(());
             } else {
@@ -40,13 +40,13 @@ async fn main() {
             if let None = err {
                 let user = message.from().unwrap();
                 let last_name = user.last_name.to_owned().unwrap_or(String::new());
-                log::info!("{} Couldn't download the video: {} From user: {} {}/{}", Local::now(), message_text, user.first_name, last_name, user.id);
+                log::info!("{} Couldn't download the video: {} From user: {} {}/{}", Local::now(), message_text, user.first_name, last_name, user.id.0);
                 bot.send_message(message.chat_id(), "Could not download the specified video, please try again later.").await?;
                 return respond(());
             }
             let user = message.from().unwrap();
             let last_name = user.last_name.to_owned().unwrap_or(String::new());
-            log::info!("{} Now processing: {} From user: {} {}/{}", Local::now(), message_text, user.first_name, last_name, user.id);
+            log::info!("{} Now processing: {} From user: {} {}/{}", Local::now(), message_text, user.first_name, last_name, user.id.0);
             let file = InputFile::file(response.get_file_name());
             let content = format!("Author: {}\nDescription: {}\nDuration: {}\nDate uploaded: {}",
                                   response.aweme_detail.author.unique_id,
@@ -64,7 +64,7 @@ async fn main() {
             if let Some(state) = user {
                 if message.chat.is_private() {
                     let last_name = state.last_name.to_owned().unwrap_or(String::new());
-                    log::info!("{} Rejected request `Not a tiktok link`: {} From user: {} {}/{}", Local::now(), message_text, state.first_name, last_name, state.id);
+                    log::info!("{} Rejected request `Not a tiktok link`: {} From user: {} {}/{}", Local::now(), message_text, state.first_name, last_name, state.id.0);
                     bot.send_message(message.chat_id(), "Not a tiktok link.").await?;
                 }
 
